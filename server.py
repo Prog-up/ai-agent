3c4fad0238a5eb0d63b3bb39c26ace3f7793b6cd
enzo.juhel <enzo.juhel@epita.fr>
2026-05-14 10:14:34 +0200

    feat: v7

--- server.py
+++ server.py
@@ -69,10 +69,7 @@ if DEVICE not in devices and DEVICE != "AUTO":
 logger.info(f"Available devices: {devices}")
 logger.info(f"Using device: {DEVICE}")
 
-ov_config = {
-    "INFERENCE_PRECISION_HINT": "f32",
-    "CACHE_DIR": "/tmp/model_cache"
-}
+ov_config = {"INFERENCE_PRECISION_HINT": "f32"}
 
 logger.info("Loading model...")
 t0 = time.time()
